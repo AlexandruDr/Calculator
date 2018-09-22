bf13e451639459090674f2577c11b4b2d2d5062e
AlexandruDr <draghicialex96@gmail.com>
2018-09-22 22:44:02 +0300

    fully functional/no img/ incomplete country data

--- CalculatorW.js
+++ CalculatorW.js
@@ -1,193 +1,989 @@
-function obtain0 (nr)
-{
-return parseInt(nr/1000000);
-}
+  var Germany = { kw: 369.6, ins: 2.75, state: "Germany"};
 
-function obtain1 (nr)
-{
-return (parseInt(nr/100000))%10;
-}
+                                var UK = {kw: 285, ins: 2.00, state: "UK"};
 
-function obtain2 (nr)
-{
-return (parseInt(nr/10000))%10;
-}
+                                var France = {kw: 345, ins: 2.75, state: "France"};
 
-function obtain3 (nr)
-{
-return (parseInt(nr/1000))%10;
-}
+                                var Denmark = {kw: 2.75, ins: 2.75, state: "Denmark"};
 
-function obtain4 (nr)
-{
-return (parseInt(nr/100))%10;
-}
+                                var Greece = {kw: 315, ins: 5.00, state: "Greece"};
 
-function obtain5 (nr)
-{
-return (parseInt(nr/10))%10;
-}
+                                var Ireland = {kw: 301.5, ins: 2.25, state: "Ireland"};
 
-function obtain6 (nr)
-{
-    return nr%10;
-}
+                                var Switzerland = {kw: 375, ins: 12.5, state: "Switzerland"};
 
-function makeKW (value)
-{
-    return ( (obtain0(value)*10 + obtain1(value) ) + ( obtain2(value)*10 + obtain3(value) )/100 );
-}
+                                var Netherlands = {kw: 358.5, ins: 11.95, state: "Netherlands"};
 
-function makeIns (value)
-{ 
-		return ( ( obtain4(value) + ( obtain5(value)*10 + obtain6(value) )/100 ) );
-}
+                                var Belgium = {kw: 358.5, ins: 2.75, state: "Belgium"};
 
-function compute (value) 
-{
-    return 1.3 * makeKW (value) / makeIns (value); 
-}
+                                var Czech = {kw: 285, ins: 9.50, state: "Czech Republic"};
 
-function produce () 
-{
-  var data = document.getElementById("selData");
-  var value = selData.value;
-  return compute(value);
-}
+                                var Croatia = {kw: 322.5, ins: 10.75, state: "Croatia"};
 
+                                var Bulgaria = {kw: 226.5, ins: 7.55, state: "Bulgaria"};
 
-function display ()
-{
-  var toInsert = produce();
-  //var insertTo = document.getElementById("userOutput");
-  document.getElementById("userOutput").innerHTML = (toInsert.toFixed(1) ) + " kW";
-}
+                                var Spain = {kw: 345, ins: 11.50, state: "Spain"};
 
-var Alabama   = { kw: 1214, ins: 5.25, state: "Alabama"};
-var Alaska   = { kw: 590, ins: 3.25, state: "Alaska"};
-var Arizona   = { kw: 1030, ins: 6.75, state: "Arizona"};
-var Arkansas   = { kw: 1083, ins: 5.25, state: "Arkansas"};
-var California = { kw: 547, ins:6.25 , state: "California"};
-var Colorado  = { kw: 694, ins:6.00 , state: "Colorado"};
-var Connecticut = { kw: 711, ins: 4.12, state: "Connecticut"};
-var Delaware    = { kw:947 , ins: 4.75, state: "Delaware"};
-var DC    = { kw: 804, ins: 4.75, state: "DC"};
-var Florida    = { kw: 1123, ins: 5.75, state: "Florida"};
-var Georgia    = { kw: 1138, ins: 5.75, state: "Georiga"};
-var Hawaii   = { kw: 505, ins: 6.25, state: "Hawaii"};
-var Idaho   = { kw: 953, ins: 4.25, state: "Idaho"};
-var Illinois= { kw: 733, ins: 4.75, state: "Illinois"};
-var Indiana = { kw: 975, ins: 4.5, state: "Indiana"};
-var Iowa= { kw: 864, ins: 4.75, state: "Iowa"};
-var Kansas   = { kw: 899, ins: 6 , state: "Kansas"};
-var Kentucky   = { kw: 1121, ins: 4.75, state: "Kentucky"};
-var Louisiana   = { kw: 1240, ins: 5.25, state: "Louisiana"};
-var Maine = { kw: 546, ins: 4.12, state: "Maine"};
-var Maryland    = { kw: 995, ins: 4.75 , state: "Maryland"};
-var Massachusetts = { kw: 599, ins: 4, state: "Massachusetts"};
-var Michigan= { kw: 668, ins: 4.25, state: "Michigan"};
-var Minnesota    = { kw: 764, ins:4.37 , state: "Minnesota"};
-var Mississippi   = { kw: 1203, ins: 5.25, state: "Mississippi"};
-var Missouri    = { kw: 1041, ins: 5.25, state: "Missouri"};
-var Montana   = { kw: 813, ins: 4.75, state: "Montana"};
-var Nebraska    = { kw: 973, ins: 5.75, state: "Nebraska"};
-var Nevada   = { kw: 925, ins: 6.25, state: "Nevada"};
-var NewHampshire = { kw: 604, ins: 4, state: "New Hampshire"};
-var NewJersey = { kw: 691, ins: 4.75, state: "New Jersey"};
-var NewMexico   = { kw: 631, ins: 6.5, state: "New Mexico"};
-var NewYork = { kw: 595, ins: 4.12, state: "New York"};
-var NorthCarolina    = { kw: 1101, ins: 4.85, state: "North Carolina"};
-var NorthDakota    = { kw: 1046, ins: 4.75, state: "North Dakota"};
-var Ohio = { kw: 891 , ins: 4.37, state: "Ohio"};
-var Oklahoma   = { kw: 1093, ins: 6, state: "Oklahoma"};
-var Oregon   = { kw: 907, ins: 4.37, state: "Oregon"};
-var Pennsylvania= { kw: 841, ins: 4.25, state: "Pennsylvania"};
-var RhodeIsland = { kw: 586 , ins: 4.12, state: "Rhode Island"};
-var SouthCarolina    = { kw: 1155, ins: 5 , state: "South Carolina"};
-var SouthDakota    = { kw: 981, ins: 5.25, state: "South Dakota"};
-var Tennessee   = { kw: 1238, ins: 4.85, state: "Tennessee"};
-var Texas   = { kw: 1156, ins: 6.25, state: "Texas"};
-var Utah   = { kw: 750, ins: 6, state: "Utah"};
-var Vermont = { kw: 549, ins: 4, state: "Vermont"};
-var Virginia  = { kw: 1120, ins: 4.75 , state: "Virginia"};
-var Washington   = { kw: 955, ins: 4.12, state: "Washington"};
-var WestVirginia   = { kw: 1102, ins: 4.37, state: "West Virginia"};
-var Wisconsin = { kw: 683, ins: 4.25, state: "Wisconsin"};
-var Wyoming   = { kw: 850, ins: 5.65, state: "Wyoming"}
+                                var Italy = {kw: 205.5, ins: 6.85, state: "Italy"};
 
-var states = [Alabama,
-Alaska ,
-Arizona ,
-Arkansas ,
-California ,
-Colorado ,
-Connecticut ,
-Delaware,
-DC, 
-Florida,
-Georgia ,
-Hawaii ,
-Idaho ,
-Illinois ,
-Indiana ,
-Iowa ,
-Kansas ,
-Kentucky ,
-Louisiana ,
-Maine ,
-Maryland ,
-Massachusetts,
-Michigan ,
-Minnesota ,
-Mississippi ,
-Missouri ,
-Montana ,
-Nebraska ,
-Nevada,
-NewHampshire ,
-NewJersey ,
-NewMexico ,
-NewYork ,
-NorthCarolina ,
-NorthDakota ,
-Ohio ,
-Oklahoma ,
-Oregon ,
-Pennsylvania ,
-RhodeIsland ,
-SouthCarolina ,
-SouthDakota,
-Tennessee ,
-Texas ,
-Utah ,
-Vermont ,
-Virginia ,
-Washington ,
-WestVirginia ,
-Wisconsin ,
-Wyoming ,
+                                var Hungary = {kw: 195, ins: 6.5, state: "Hungary"};
+
+                                var Romania = {kw: 127.5, ins: 3.75, state: "Romania"};
+
+                                var Poland = {kw: 135, ins: 2.75, state: "Poland"};
+
+                                var Norway = {kw: 1657.5, ins: 2.25, state: "Norway"};
+
+                                var Sweden = {kw: 975, ins: 2.50, state: "Sweden"};
+
+                                var Finland = {kw: 817.5, ins: 27.25, state: "Finland"};
+
+                                var Lithuania = {kw: 211.5, ins: 7.05, state: "Lithuania"};
+
+                                var Latvia = {kw: 217.5, ins: 7.25, state: "Latvia"};
+
+                                var Estonia = {kw: 223.5, ins: 7.45, state: "Estonia"};
+
+                                var Austria = {kw: 346.5, ins: 11.55, state: "Austria"};
+
+                                var Belgium = {kw: 337.5, ins: 11.25, state: "Belgium"};
+
+                                var Malta = {kw: 202.5, ins: 6.75, state: "Malta"};
+
+                                var Cyprus = {kw:202.5, ins: 6.75, state: "Cyprus"};
+
+                                var Portugal = { kw:325.5, ins: 10.85, state: "Portugal"};
+
+                                var Slovenia = {kw:310.5, ins: 3.25, state: "Slovenia"};
+
+                 
+
+                  
+
+                  var Alabama = {
+
+        kw: 1214,
+
+        ins: 5.25,
+
+        state: "Alabama"
+
+      };
+
+ 
+
+      var Alaska = {
+
+        kw: 590,
+
+        ins: 3.25,
+
+        state: "Alaska"
+
+      };
+
+ 
+
+      var Arizona = {
+
+        kw: 1030,
+
+        ins: 6.75,
+
+        state: "Arizona"
+
+      };
+
+ 
+
+      var Arkansas = {
+
+        kw: 1083,
+
+        ins: 5.25,
+
+        state: "Arkansas"
+
+      };
+
+ 
+
+      var California = {
+
+        kw: 547,
+
+        ins: 6.25,
+
+        state: "California"
+
+      };
+
+ 
+
+      var Colorado = {
+
+        kw: 694,
+
+        ins: 6.00,
+
+        state: "Colorado"
+
+      };
+
+ 
+
+      var Connecticut = {
+
+        kw: 711,
+
+        ins: 4.12,
+
+        state: "Connecticut"
+
+      };
+
+ 
+
+      var Delaware = {
+
+        kw: 947,
+
+        ins: 4.75,
+
+       state: "Delaware"
+
+      };
+
+ 
+
+      var DC = {
+
+        kw: 804,
+
+        ins: 4.75,
+
+        state: "DC"
+
+      };
+
+ 
+
+      var Florida = {
+
+        kw: 1123,
+
+        ins: 5.75,
+
+        state: "Florida"
+
+      };
+
+ 
+
+      var Georgia = {
+
+        kw: 1138,
+
+        ins: 5.75,
+
+        state: "Georgia"
+
+      };
+
+ 
+
+      var Hawaii = {
+
+        kw: 505,
+
+        ins: 6.25,
+
+        state: "Hawaii"
+
+      };
+
+ 
+
+      var Idaho = {
+
+        kw: 953,
+
+        ins: 4.25,
+
+        state: "Idaho"
+
+      };
+
+ 
+
+      var Illinois = {
+
+        kw: 733,
+
+        ins: 4.75,
+
+        state: "Illinois"
+
+      };
+
+ 
+
+      var Indiana = {
+
+        kw: 975,
+
+        ins: 4.5,
+
+        state: "Indiana"
+
+      };
+
+ 
+
+      var Iowa = {
+
+        kw: 864,
+
+        ins: 4.75,
+
+        state: "Iowa"
+
+      };
+
+ 
+
+      var Kansas = {
+
+        kw: 899,
+
+        ins: 6,
+
+        state: "Kansas"
+
+      };
+
+ 
+
+      var Kentucky = {
+
+        kw: 1121,
+
+        ins: 4.75,
+
+        state: "Kentucky"
+
+      };
+
+ 
+
+      var Louisiana = {
+
+        kw: 1240,
+
+        ins: 5.25,
+
+        state: "Louisiana"
+
+      };
+
+ 
+
+      var Maine = {
+
+        kw: 546,
+
+        ins: 4.12,
+
+        state: "Maine"
+
+      };
+
+ 
+
+      var Maryland = {
+
+        kw: 995,
+
+        ins: 4.75,
+
+        state: "Maryland"
+
+      };
+
+ 
+
+      var Massachusetts = {
+
+        kw: 599,
+
+        ins: 4,
+
+        state: "Massachusetts"
+
+      };
+
+ 
+
+      var Michigan = {
+
+        kw: 668,
+
+        ins: 4.25,
+
+        state: "Michigan"
+
+      };
+
+ 
+
+      var Minnesota = {
+
+        kw: 764,
+
+        ins: 4.37,
+
+        state: "Minnesota"
+
+      };
+
+ 
+
+      var Mississippi = {
+
+        kw: 1203,
+
+        ins: 5.25,
+
+        state: "Mississippi"
+
+      };
+
+ 
+
+      var Missouri = {
+
+        kw: 1041,
+
+        ins: 5.25,
+
+        state: "Missouri"
+
+      };
+
+ 
+
+      var Montana = {
+
+        kw: 813,
+
+        ins: 4.75,
+
+        state: "Montana"
+
+      };
+
+ 
+
+      var Nebraska = {
+
+        kw: 973,
+
+        ins: 5.75,
+
+        state: "Nebraska"
+
+      };
+
+ 
+
+      var Nevada = {
+
+        kw: 925,
+
+        ins: 6.25,
+
+        state: "Nevada"
+
+      };
+
+ 
+
+      var NewHampshire = {
+
+        kw: 604,
+
+        ins: 4,
+
+        state: "New Hampshire"
+
+      };
+
+ 
+
+      var NewJersey = {
+
+        kw: 691,
+
+        ins: 4.75,
+
+        state: "New Jersey"
+
+      };
+
+ 
+
+      var NewMexico = {
+
+        kw: 631,
+
+        ins: 6.5,
+
+        state: "New Mexico"
+
+      };
+
+ 
+
+      var NewYork = {
+
+        kw: 595,
+
+        ins: 4.12,
+
+        state: "New York"
+
+      };
+
+ 
+
+      var NorthCarolina = {
+
+        kw: 1101,
+
+        ins: 4.85,
+
+        state: "North Carolina"
+
+      };
+
+ 
+
+      var NorthDakota = {
+
+        kw: 1046,
+
+        ins: 4.75,
+
+        state: "North Dakota"
+
+      };
+
+ 
+
+      var Ohio = {
+
+        kw: 891,
+
+        ins: 4.37,
+
+        state: "Ohio"
+
+      };
+
+ 
+
+      var Oklahoma = {
+
+        kw: 1093,
+
+        ins: 6,
+
+        state: "Oklahoma"
+
+      };
+
+ 
+
+      var Oregon = {
+
+        kw: 907,
+
+        ins: 4.37,
+
+        state: "Oregon"
+
+      };
+
+ 
+
+      var Pennsylvania = {
+
+        kw: 841,
+
+        ins: 4.25,
+
+        state: "Pennsylvania"
+
+      };
+
+ 
+
+      var RhodeIsland = {
+
+        kw: 586,
+
+        ins: 4.12,
+
+        state: "Rhode Island"
+
+      };
+
+ 
+
+      var SouthCarolina = {
+
+        kw: 1155,
+
+        ins: 5,
+
+        state: "South Carolina"
+
+      };
+
+ 
+
+      var SouthDakota = {
+
+        kw: 981,
+
+        ins: 5.25,
+
+        state: "South Dakota"
+
+     };
+
+ 
+
+      var Tennessee = {
+
+        kw: 1238,
+
+        ins: 4.85,
+
+        state: "Tennessee"
+
+      };
+
+ 
+
+      var Texas = {
+
+        kw: 1156,
+
+        ins: 6.25,
+
+        state: "Texas"
+
+      };
+
+ 
+
+      var Utah = {
+
+        kw: 750,
+
+        ins: 6,
+
+        state: "Utah"
+
+      };
+
+ 
+
+      var Vermont = {
+
+        kw: 549,
+
+        ins: 4,
+
+        state: "Vermont"
+
+      };
+
+ 
+
+      var Virginia = {
+
+        kw: 1120,
+
+        ins: 4.75,
+
+        state: "Virginia"
+
+      };
+
+ 
+
+      var Washington = {
+
+        kw: 955,
+
+        ins: 4.12,
+
+        state: "Washington"
+
+      };
+
+ 
+
+      var WestVirginia = {
+
+        kw: 1102,
+
+        ins: 4.37,
+
+        state: "West Virginia"
+
+      };
+
+ 
+
+      var Wisconsin = {
+
+        kw: 683,
+
+        ins: 4.25,
+
+        state: "Wisconsin"
+
+      };
+
+ 
+
+      var Wyoming = {
+
+        kw: 850,
+
+        ins: 5.65,
+
+        state: "Wyoming"
+
+      }
+
+ 
+
+ 
+
+ 
+
+      var states = [
+
+                Alabama,
+
+                Alaska,
+
+                Arizona,
+
+                Arkansas,
+
+                California,
+
+                Colorado,
+
+                Connecticut,
+
+                Delaware,
+
+                DC,
+
+                Florida,
+
+                Georgia,
+
+                Hawaii,
+
+                Idaho,
+
+                Illinois,
+
+                Indiana,
+
+                Iowa,
+
+                Kansas,
+
+                Kentucky,
+
+                Louisiana,
+
+                Maine,
+
+                Maryland,
+
+                Massachusetts,
+
+                Michigan,
+
+                Minnesota,
+
+                Mississippi,
+
+                Missouri,
+
+                Montana,
+
+                Nebraska,
+
+                Nevada,
+
+                NewHampshire,
+
+                NewJersey,
+
+                NewMexico,
+
+                NewYork,
+
+                NorthCarolina,
+
+                NorthDakota,
+
+                Ohio,
+
+                Oklahoma,
+
+                Oregon,
+
+                Pennsylvania,
+
+                RhodeIsland,
+
+                SouthCarolina,
+
+                SouthDakota,
+
+                Tennessee,
+
+                Texas,
+
+                Utah,
+
+                Vermont,
+
+                Virginia,
+
+                Washington,
+
+                WestVirginia,
+
+                Wisconsin,
+
+                Wyoming,
+
+                Austria,
+
+                Belgium,
+
+                Bulgaria,
+
+                Croatia,
+
+                Cyprus,
+
+                Czech,
+
+                Denmark,
+
+                Estonia,
+
+                Finland,
+
+                France,
+
+                Germany,
+
+                Greece,
+
+                Hungary,
+
+                Ireland,
+
+                Italy,
+
+                Latvia,
+
+                Lithuania,
+
+                Malta,
+
+                Netherlands,
+
+                Poland,
+
+                Portugal,
+
+                Romania,
+
+                Slovenia,
+
+                Spain,
+
+                Sweden,
+
+                UK
+
 ];
 
-function addOptions ()
-{
-    var i = 0;
-  for (i=0; i<states.length; i++)
-  {
-    var option = document.createElement("option");
-    option.value = ( parseFloat( (states[i].kw/30).toFixed(2)) ) * 100000 + (states[i].ins)*100;
-    option.innerHTML = states[i].state;
-    document.getElementById("selData").appendChild(option);
-  }
-}
+     
 
-addOptions();
+ 
 
-function clear ()
-{
-    var sel = document.getElementById("selData");
-    sel.addEventListener("click", function () {
-        document.getElementById("userOutput").innerHTML = "Your System's Power Output";
-    } )
-}
+      function addOptions()
 
-clear();
+ 
+
+      {
+
+        document.getElementById("selData").innerHTML = '';
+
+ 
+
+        var i = 0;
+
+ 
+
+        for (i = 0; i < states.length; i++)
+
+ 
+
+        {
+
+ 
+
+          var option = document.createElement("option");
+
+ 
+
+          option.value = (parseFloat((states[i].kw / 30).toFixed(2))) * 100000 + (states[i].ins) * 100;
+
+ 
+
+          option.innerHTML = states[i].state;
+
+ 
+
+          option.id = states[i].state;
+
+ 
+
+          document.getElementById("selData").appendChild(option);
+
+ 
+
+        }
+
+ 
+
+      }
+
+ 
+
+      addOptions();
+
+ 
+
+      function inputExtract()
+
+      {
+
+        var pointer = document.getElementById("selData").selectedIndex;
+
+        var selData = document.getElementById("selData");
+
+                                var input = document.getElementById("input").value;
+
+        selData[pointer].value = input * 100000 + ((selData[pointer].value) % 1000);
+
+      }
+
+                               
+
+                                function inputExtract2()
+
+      {
+
+        var pointer = document.getElementById("selData").selectedIndex;
+
+        var selData = document.getElementById("selData");
+
+                                var input = document.getElementById("input1").value;
+
+        selData[pointer].value =( parseInt( (selData[pointer].value)/1000) )*1000 + input*100;
+
+      }
+
+      function display()
+
+      {
+
+        var value1 = selData.value;
+
+                               
+
+        document.getElementById("userOutput").innerHTML = ((((parseInt(value1 / 1000) / 100) / ((value1 % 1000) / 100)) * 1.33).toFixed(2)) + " kW or " + ((((((parseInt(value1 / 1000) / 100) / ((value1 % 1000) / 100)) * 1.33).toFixed(2))) * 1000 / 320).toFixed(0) + " solar panels at 320W output each";
+
+               
+
+      }
+
+ 
+
+      function reset()
+
+      {
+
+        addOptions();
+
+        document.getElementById("input").value = 'Average kW/day';
+
+                                document.getElementById("input1").value = 'Insulation';
+
+        document.getElementById("userOutput").innerHTML = "Your System's Needed Power Output";
+
+               
+
+      }
+
+                 
+
+                  function clear ()
+
+                  {
+
+                                  document.getElementById("input").value = 'Average kW/day';
+
+                  }
+
+                 
+
+                  
+
+                                 document.addEventListener ("click", function ()
+
+                                 {if ( (document.getElementById("input").value == '' && event.target != document.getElementById("input")  ) ) document.getElementById("input").value = 'Average kW/day';
+
+                                 if ( (document.getElementById("input1").value == '' && event.target != document.getElementById("input1")  ) ) document.getElementById("input1").value = 'Insulation';
+
+                                }
+
+                               
+
+                                 );
+
+                 
+
+                  document.getElementById("selData").onclick = function () { if (document.getElementById("selData").selectedIndex > 50) {document.getElementById("ins").src = "insolationeurope.png";}
+
+                  
+
+                  else {document.getElementById("ins").src = "insulation.png"}; }
